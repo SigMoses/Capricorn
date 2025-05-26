23fa2cb0b5977a50f23c9b2c793c0f740ea8aa47
Moses™ <mosheanewman@gmail.com>
2025-05-25 20:18:40 -0400

    Update __init__.py

--- Capricorn_AI/__init__.py
+++ Capricorn_AI/__init__.py
@@ -5,11 +5,14 @@ Capricorn AI package: public API definitions.
 
 from .models import load_model, list_models
 from .utils import load_image, preprocess_input, predict
+from .labels import ALL_LABELS, NUM_CLASSES
 
-__all__ = [
+__all__ += [
     'load_model',
     'list_models',
     'load_image',
     'preprocess_input',
     'predict',
+    'ALL_LABELS',
+    'NUM_CLASSES',
 ]
